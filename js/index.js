b00015afb3e3193258aa24f949140fd238d0e26e
Shakshor <shakshor584@gmail.com>
2022-09-05 18:02:07 +0600

    stop watch design done

---
+++ js/index.js
@@ -0,0 +1,27 @@
+let hour = 0,
+    min = 0,
+    sec = 0,
+    count = 0;
+
+let timer = false;
+
+// start function
+function start() {
+    timer = true;
+    stopWatch();
+};
+
+// stop function
+function stop() {
+
+};
+
+// reset function
+function reset() {
+
+};
+
+// final function
+function stopWatch() {
+
+};
